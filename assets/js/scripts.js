1022bab4458ced607b5d20e40e08545c444b069e
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2024-12-20 14:52:18 +0100

    Move JavaScript code to separate js file in assest folder

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+/*  This JavaScript code ensures that the Bootstrap navbar collapses when navigating within the page */
+document.querySelectorAll(".navbar-collapse .nav-link").forEach((link) => {
+	link.addEventListener("click", function (e) {
+		let section = document.querySelector(e.target.getAttribute("href"));
+		if (section) {
+			e.preventDefault(); // Prevent default anchor click behavior
+			let navbarHeight = document.querySelector(".navbar-toggler").offsetHeight;
+			window.scroll({
+				top: section.offsetTop - navbarHeight, // Adjust for navbar height
+				behavior: "smooth",
+			});
+			document.querySelector(".navbar-collapse").classList.remove("show"); // Collapse navbar
+		}
+	});
+});
